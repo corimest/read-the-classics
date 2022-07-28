2109b4e157d595f9dca0c17b7f5bd34f43382af1
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-07-27 22:02:18 -0500

    html connection routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
+app.use(express.static('public')); 
 
 //query function
 function filterByQuery(query, booksArray) {
@@ -106,6 +107,11 @@ app.post('/api/books', (req, res) => {
     res.json(book); 
     }
 });
+
+//route for html 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html')); 
+}); 
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
