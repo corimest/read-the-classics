ae3305824c4342b48b1de368612e97d8b265ccf9
Shuai Zuo <szuo1@vols.utk.edu>
2022-07-29 08:29:44 -0500

    add models files

---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+      id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      comment_text: {
+          type: DataTypes.TEXT('long'),
+          allowNull: false,
+          len: [1]
+      },
+      user_id: {
+        type: DataTypes.INTEGER, 
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+      },
+      book_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'book',
+          key: 'id'
+        }
+      }
+    },   
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,29 @@
-// // import models
-// const  = require('./');
+const User = require('./User');
+const Book = require('./Book');
+const Comment = require('./Comment');
 
-// module.exports = {
-//  // ...models
-// };
+User.hasMany(Book, {
+	foreignKey : 'user_id'
+});
+
+Book.belongsTo(User, {
+	foreignKey : 'user_id'
+});
+
+Comment.belongsTo(User, {
+	foreignKey : 'user_id'
+});
+
+Comment.belongsTo(Book, {
+	foreignKey : 'book_id'
+});
+
+User.hasMany(Comment, {
+	foreignKey : 'user_id'
+});
+
+Book.hasMany(Comment, {
+	foreignKey : 'book_id'
+});
+
+module.exports = { User, Book, Comment };
