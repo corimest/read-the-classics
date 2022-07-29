d873780f00b49b6ccd711c29fe3ec5838574eb8d
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-07-28 20:02:03 -0500

    static files debug

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
-app.use(express.static('public')); 
+app.use(express.static(__dirname + 'public')); 
 
 //query function
 function filterByQuery(query, booksArray) {
